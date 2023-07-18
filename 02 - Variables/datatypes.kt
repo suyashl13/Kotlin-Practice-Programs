b235428ca379bf558b7deaa4c494a2af954b4894
Suyash Lawand (DEL) <suyash.lawand@gmail.com>
2023-07-18 18:21:10 +0530

    add: 03 - Variables

---
+++ 02 - Variables/datatypes.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    // String
+    val myName: String = "Suyash"
+
+    // Integer
+    var age: Int = 22
+
+    // Double
+    var height: Double = 172.36 
+
+    // Boolean
+    val isMale: Boolean = true
+
+
+    // Print
+    println("Name: "+myName)
+    println("Age: "+age)
+    println("Height: "+height)
+    println("Is Male: "+isMale)
+}
---
+++ 02 - Variables/vartypes.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // This is constant string;
+    val myName: String = "Suyash Lawand"
+    
+    // This is variable
+    var age: Int = 22
+  	age = age + 1 // It is mutable
+    
+    print(age)
+}
